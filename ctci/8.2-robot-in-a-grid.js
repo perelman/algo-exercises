320a42c1e24096daf3347b0c8827122094132e45
perelman yuval <perelman.yuval@gmail.com>
2021-10-29 09:34:48 +0300

    add test

---
+++ ctci/8.2-robot-in-a-grid.js
@@ -0,0 +1,44 @@
+//We need to go from R to T
+//We can move R and D only
+//In order to check all the paths we need 
+//A. Loop over the grid each step at a time.
+//For each step we need to look if we can move right or down or both
+//We need to run the check function on every allowed dir
+
+/**
+ * R,A,A,A,A,A,A
+ * N,N,A,A,A,A,A
+ * A,A,A,N,A,A,A
+ * N,N,A,N,N,N,A
+ * N,N,A,A,A,A,N
+ * N,N,N,A,A,A,T
+*/
+
+const findPath = grid => {
+    
+};
+module.exports = {
+    findPath
+}
+
+
+/**
+ * R,A,A,A,A,A,A
+ * N,N,A,A,A,A,A
+ * A,A,A,N,A,A,A
+ * N,N,A,N,N,N,A
+ * N,N,N,A,A,A,A
+ * N,N,N,A,A,A,T
+*/
+/**
+ * R,A,A,A,A,A,A
+ * N,N,A,A,A,A,A
+ * A,A,A,N,A,A,A
+ * N,N,A,N,N,N,A
+ * N,N,N,A,A,A,N
+ * N,N,N,A,A,A,T
+*/
+
+
+
+
